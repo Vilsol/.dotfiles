f9a0fcb8e39b342d008588e0807e29b2908d37ca
Vilsol <me@vil.so>
2024-03-19 22:50:19 +0200

    remove age

--- system/os.nix
+++ system/os.nix
@@ -1,8 +1,4 @@
 {
-  age,
-  config,
-  ...
-}: {
   nix = {
     optimise.automatic = true;
 
@@ -13,15 +9,5 @@
       dates = "weekly";
       options = "--delete-older-than 14d";
     };
-  };
-
-  age.identityPaths = [
-    "/home/vilsol/.ssh/id_25519"
-  ];
-
-  age.secrets.secret1.file = ../secrets/secret1.age;
-
-  environment.etc.foo = {
-    text = config.age.secrets.secret1.path;
   };
 }
