0d8af156d2189aa5eb0c1627d90ebb8790618828
Vilsol <me@vil.so>
2025-01-31 12:03:31 +0200

    remove duplicate driver

--- system/kernel.nix
+++ system/kernel.nix
@@ -14,8 +14,7 @@
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      intel-media-driver
       vaapiVdpau
       libvdpau-va-gl
     ];
